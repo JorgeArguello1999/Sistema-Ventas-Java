0684e71eb310ea018c5e1aa745c695ca0d8c82be
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-01-30 11:58:43 -0500

    Tabla Clientes bloqueada

--- src/Vista/Sistema.java
+++ src/Vista/Sistema.java
@@ -826,7 +826,15 @@ public final class Sistema extends javax.swing.JFrame {
             new String [] {
                 "ID", "DNI/RUC", "NOMBRE", "TELÉFONO", "DIRECCIÓN"
             }
-        ));
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
         TableCliente.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 TableClienteMouseClicked(evt);
